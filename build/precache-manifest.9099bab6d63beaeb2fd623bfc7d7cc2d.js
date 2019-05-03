ef00d1afad085eebec94eaefef2c8102312e4070
ghost <bilobom124@gmail.com>
2019-05-03 23:46:08 +0100

    first commit :tada:

---
+++ build/precache-manifest.9099bab6d63beaeb2fd623bfc7d7cc2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ffea9b8b3973e31a21b8336627a3e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9577441aae604c762a64",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "b301699af4c8b13aac8b",
+    "url": "/static/js/2.8c895fe0.chunk.js"
+  },
+  {
+    "revision": "9577441aae604c762a64",
+    "url": "/static/js/main.66526907.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ controllers/socket.js
@@ -0,0 +1,25 @@
+
+module.exports=(server)=>{
+    var clients=[]
+    var io = require('socket.io')(server)
+
+    io.on('connection', function (client) {
+        clients.push(client.id);
+        console.log('new client', client.id)
+        client.on('disconnect', function () {
+            clients = clients.filter(item => item !== client.id)
+            console.log('user disconnected',clients);
+        });
+        setTimeout(() => {
+            client.emit('data','hello this is data from mars are you listnening?')
+        }, 5000);
+
+        client.on('wantToSend',(recieverId,data)=>{
+            console.log('wantToSend event came for', recieverId)
+            
+            //io.to(recieverId).emit('data', data+ 'from : '+ client.id);
+            if (clients.includes(recieverId))
+             io.sockets.connected[recieverId].emit('data',data)
+        })
+    });
+}
